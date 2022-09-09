f3736a5fdeeacdccfdc992be4d3b36f8f2748600
paigni <ikontai4@gmail.com>
2022-09-09 20:21:19 +0600

    Добавлены чекм
    Добавлены чеки

---
+++ check.py
@@ -0,0 +1,23 @@
+def check_start_choice(inp):
+    if 0 < int(inp) < 3:
+        return True
+    return False
+
+
+def check_hero_choice(inp):
+    if 1 <= int(inp) <= 3:
+        return True
+    return False
+
+def check_monster_choice(inp, le_of_lis):
+    if le_of_lis >= inp:
+        return True
+    return False
+
+
+def check_hero_energy(mana):
+    if mana >= 0:
+        return True
+    return False
+
+
