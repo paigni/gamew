ff1fc9eb0f61fa9105332be64ae8a838855d3e9c
paigni <ikontai4@gmail.com>
2022-09-12 19:59:11 +0600

    Update service.py
    Перенесена функция из интерфейса, добавлены докстринги

--- service.py
+++ service.py
@@ -2,7 +2,7 @@ import random
 
 from monster_classes import Rat, Goblin, Dragon, BigGoblin, DragonsBaby
 from hero_classes import Mage, Paladin, Priest
-
+from check import check_hero_mana
 
 rat = Rat()
 goblin = Goblin()
@@ -11,9 +11,7 @@ big_goblin = BigGoblin()
 dragons_baby = DragonsBaby()
 
 list_of_monsters = [rat, goblin, dragon, big_goblin, dragons_baby]
-amount_monsters = random.randint(1,4)
-
-
+amount_monsters = random.randint(2, 5)
 
 mage = Mage()
 paladin = Paladin()
@@ -21,3 +19,16 @@ priest = Priest()
 list_of_heroes = [mage, paladin, priest]
 
 
+def defeated_monster(hero_list):
+    """
+    Функция добавляет 10 маны всем героям, если их мана не больше максимальной
+
+    Args:
+        hero_list: список героев
+
+    Returns:
+    """
+    for hero in hero_list:
+        hero.current_mana += 10
+        if check_hero_mana(hero):
+            hero.current_mana = hero.max_mana
