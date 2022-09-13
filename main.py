fee0452a2e03e059b41e31c0eda4ca05e3a0153b
paigni <ikontai4@gmail.com>
2022-09-13 20:25:12 +0600

    Update main.py
    Рефакторинг основной функции

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import time
 from check import check_list_of_hero
+
 from service import list_of_heroes, \
     amount_monsters, \
-    list_of_monsters
-from interface import show_hero, damage_or_ability, show_monster, block_hero
+    list_of_monsters,\
+    block_hero,\
+    defeated_monster
+
+from interface import show_hero, \
+    damage_or_ability, \
+    show_monster,\
+    choice_hero,\
+    choice_monster
+
 
 
 def main():
@@ -11,21 +20,27 @@ def main():
     monsters = list_of_monsters[:amount_monsters]
 
     while True:
+
         if not check_list_of_hero(copy_list_heroes):
-            hero = show_hero(copy_list_heroes)
+            show_hero(copy_list_heroes)
+            hero = choice_hero(copy_list_heroes)
+            hero.ability()
             hero_choice = damage_or_ability()
+            show_monster(monsters)
+
             if hero_choice == 1:
-                hero.hero_damage(show_monster(monsters, list_of_heroes))
+                hero.hero_damage(choice_monster(monsters))
             elif hero_choice == 2:
-                hero.ability()
                 if hero.name == "Paladin":
                     hero.holy_land(monsters)
                 elif hero.name == "Mage":
-                    hero.fire_ball(show_monster(monsters, list_of_heroes))
+                    hero.fire_ball(choice_monster(monsters))
                 elif hero.name == "Priest":
                     hero.healing_light(list_of_heroes)
-            copy_list_heroes.remove(hero)
-            block_hero(hero)
+
+            monsters = defeated_monster(list_of_heroes, monsters)
+
+            block_hero(hero, copy_list_heroes)
 
         else:
             for monster in monsters:
